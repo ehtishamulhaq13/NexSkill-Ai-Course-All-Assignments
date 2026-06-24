904c7f51d9e98ad11135b4e5e8973a1ed0ceb284
ehtishamulhaq13 <ehtishamidrees49@gmail.com>
2026-06-24 18:36:29 +0500

    data structure tuple

---
+++ practice data structure tuple.py
@@ -0,0 +1,9 @@
+book_tuple=(456, "thinking glow rich", "nipolian hil", 44.0)
+print(book_tuple)
+print(type(book_tuple))
+print(len(book_tuple))
+
+for x in book_tuple:
+    print(x)
+
+print(book_tuple[2])
